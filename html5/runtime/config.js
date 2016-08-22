70d1098420dd94350325767d37b60a9993570e4a
xinran.lxr <xinran.lxr@alibaba-inc.com>
2016-08-22 14:03:05 +0800

    chore: init project file

---
+++ html5/runtime/config.js
@@ -0,0 +1,9 @@
+// built by npm run build:config
+
+import * as Vue from 'weex-vue-framework'
+import * as Weex from '../default'
+
+export default {
+  Vue,
+  Weex
+}
---
+++ src/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <example-list :items="items" dir="src"></example-list>
+</template>
+
+<script>
+  module.exports = {
+    components: {
+      'example-list': require('./include/example-list.vue')
+    },
+    data: {
+      items: [
+        // `name` key is the example filename without '.vue'
+        {name: 'syntax/hello-world', title: 'Hello World'},
+        {name: 'showcase/itemlist', title: 'List'},
+        {name: 'showcase/calculator', title: 'Calculator'},
+        {name: 'showcase/new-fashion', title: 'Activity Page'}
+      ]
+    }
+  }
+</script>
